f9a5419ccb480ce6f9c24cc16ebc6fbf9276a53b
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2022-01-28 22:42:20 -0500

    DEBUG: Log github tablet service

--- OpenTabletDriver.Web.Core/GitHub/Services/GitHubTabletService.cs
+++ OpenTabletDriver.Web.Core/GitHub/Services/GitHubTabletService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OpenTabletDriver.Web.Core.Services;
 
 namespace OpenTabletDriver.Web.Core.GitHub.Services
@@ -12,11 +13,17 @@ namespace OpenTabletDriver.Web.Core.GitHub.Services
     {
         private readonly IServiceProvider serviceProvider;
         private readonly IReleaseService releaseService;
+        private readonly ILogger<GitHubTabletService> logger;
 
-        public GitHubTabletService(IServiceProvider serviceProvider, IReleaseService releaseService)
+        public GitHubTabletService(
+            IServiceProvider serviceProvider,
+            IReleaseService releaseService,
+            ILogger<GitHubTabletService> logger
+        )
         {
             this.serviceProvider = serviceProvider;
             this.releaseService = releaseService;
+            this.logger = logger;
 
             GetMarkdownCached = new CachedTask<string>(GetMarkdownRawInternal, CacheTime);
         }
@@ -32,14 +39,23 @@ namespace OpenTabletDriver.Web.Core.GitHub.Services
 
         public async Task<string> GetMarkdownRawInternal()
         {
+            logger.Log(LogLevel.Information, "Fetching Tablets table...");
+
             var repoContent = await releaseService.GetRepositoryContent();
+            logger.Log(LogLevel.Information, "Fetched repository content");
+
             var file = repoContent.First(r => r.Name == "TABLETS.md");
+            logger.Log(LogLevel.Information, $"Found TABLETS.md: {file.Size} bytes");
 
             using (var httpClient = serviceProvider.GetRequiredService<HttpClient>())
             using (var httpStream = await httpClient.GetStreamAsync(file.DownloadUrl))
             using (var sr = new StreamReader(httpStream))
             {
-                return await sr.ReadToEndAsync();
+                logger.Log(LogLevel.Information, "Downloading TABLETS.md...");
+                var content = await sr.ReadToEndAsync();
+
+                logger.Log(LogLevel.Information, "Successfully downloaded TABLETS.md");
+                return content;
             }
         }
     }
